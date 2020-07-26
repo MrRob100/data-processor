4d96ce4b1e423a5ade67a62f01c35a235f5973f4
horidhenry <robarnold100@hotmail.com>
2020-07-26 18:16:47 +0100

    service added again

---
+++ src/DataProcess.php
@@ -0,0 +1,30 @@
+<?php
+
+// declare(strict_types=1);
+
+namespace Roba\DataProcessor;
+
+/**
+ * Class DataProcess
+ * @package App\Services
+ */
+class DataProcess
+{
+    /**
+     * @param array $coins - all coins listed on exchange 
+     *
+     * @return o
+     */
+    public function load()
+    {
+      dump('in load');
+    }
+
+    public function other()
+    {
+      //
+    }
+
+
+
+}
